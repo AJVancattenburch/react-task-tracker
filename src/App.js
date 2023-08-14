f6e656ccf599d2dea83fdefaf16a27e233d2f319
Anthony Vancattenburch <wanderwithme1988@gmail.com>
2023-08-14 13:45:19 -0600

    form toggling between show/hide

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import AddTask from "./components/AddTask"
 
 function App() {
 
+  const [showAddTask, setShowAddTask] = useState(false)
+
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -26,6 +28,13 @@ function App() {
     }
   ])
 
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
   //Delete Task
   const deleteTask = (id) => {
     setTasks(
@@ -45,8 +54,12 @@ function App() {
 
   return (
     <div className="container">
-      <Header props="" />
-      <AddTask />
+      <Header 
+        onAdd={() => 
+          setShowAddTask(!showAddTask)} 
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? 
         <Tasks 
           tasks={tasks} 
